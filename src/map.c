49e9ae7248d6bae7a7a1a0622615c048fc503a2c
FireAid101 <aidenkoorsen14@gmail.com>
2023-05-25 21:54:11 +0200

    Added map rendering. Need to fix memory leak

--- src/map.c
+++ src/map.c
@@ -1,10 +1,43 @@
 #include "map.h"
 
+// Camera Handle function -- DO NOT USE IN GAME - THIS IS JUST UNTIL I HAVE A PLAYER
+void HandleCamera(Camera *ptr_camera)
+{
+    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_LEFT])
+    {
+        ptr_camera->x -= 5;
+        ptr_camera->dx += 5;
+    }
+
+    if (key_state[SDL_SCANCODE_RIGHT])
+    {
+        ptr_camera->x += 5;
+        ptr_camera->dx -= 5;        
+    }
+    
+    if (key_state[SDL_SCANCODE_UP])
+    {
+        ptr_camera->y -= 5;
+        ptr_camera->dy += 5;    
+    }
+
+    if (key_state[SDL_SCANCODE_DOWN])
+    {
+        ptr_camera->y += 5;
+        ptr_camera->dy -= 5;    
+    }
+}
+
 // Creates and loads the whole tilemap
-TileMap LoadTileMap(char *ptr_mapPath)
+TileMap LoadTileMap(char *ptr_mapPath, State *ptr_state)
 {
     TileMap result;
     result.amountOfTiles = 0;
+
+    // Load the tileset
+    result.mapTileSet = LoadTexture("rsrc/img/testTileset.png", ptr_state);
 
     // First load the tilemap info
     char *path = malloc(strlen(SDL_GetBasePath()) + strlen("rsrc/data/tmi.inf"));
@@ -97,8 +130,53 @@ TileMap LoadTileMap(char *ptr_mapPath)
     FILE *fptr_mapFile = fopen(path, "r");
 
     if (fptr_mapFile != NULL)
-    {
+    {      
+        // Get map data
+        char temp[30];
+
+        // Get width and height on the first two lines
         
+        fgets(temp, 30, fptr_mapFile);
+        
+        result.width = atoi(temp);
+
+        fgets(temp, 30, fptr_mapFile);
+        result.height = atoi(temp);
+
+        // Now allocate the sizes to the map
+        result.dst_rects = malloc(result.height);
+        result.isSolid = malloc(result.height);
+        result.src_rects = malloc(result.height);
+
+        for (int i = 0; i < result.height; i++)
+        {
+            result.dst_rects[i] = malloc(result.width * sizeof(SDL_Rect));
+            result.src_rects[i] = malloc(result.width * sizeof(SDL_Rect));
+            result.isSolid[i] = malloc(result.width * sizeof(bool));
+        }           
+
+        for (int y = 0; y < result.height; y++)
+        {
+            char row[(result.width * 2) + 1];
+
+            fgets(row, (result.width * 2) + 1, fptr_mapFile);
+
+            // Loop through all tiles
+            for (int x = 0; x < result.width; x++)
+            {
+                int index = atoi(&row[x * 2]);
+
+                // Set position
+                result.dst_rects[y][x].w = 16;
+                result.dst_rects[y][x].h = 16;
+                result.dst_rects[y][x].x = 16 * x;
+                result.dst_rects[y][x].y = 16 * y;
+
+                // Use of dynamic loading system with that tile infomation file - gonna save me a lot of time
+                result.src_rects[y][x] = result.ptr_mapTileInfo[index].src;
+                result.isSolid[y][x] = result.ptr_mapTileInfo[index].isSolid;
+            }
+        }
     }
     else
     {
@@ -112,7 +190,30 @@ TileMap LoadTileMap(char *ptr_mapPath)
     return result;
 }
 
+void DrawMap(TileMap map, State *ptr_state, Camera *ptr_camera)
+{
+    for (int y = 0; y < 20; y++)
+    {
+        for(int x = 0; x < 25; x++)
+        {
+            if (&map.dst_rects[ptr_camera->y + y][ptr_camera->x + x] != NULL)
+            {
+                SDL_Rect rct = map.dst_rects[ptr_camera->y + y][ptr_camera->x + x];
+               
+                rct.x += ptr_camera->dx;
+                rct.y += ptr_camera->dy;
+            
+                SDL_RenderCopy(ptr_state->ptr_renderer, map.mapTileSet.ptr_data, &map.src_rects[ptr_camera->y + y][ptr_camera->x + x], &rct);
+            }
+        }
+    }
+}
+
 void UnloadTileMap(TileMap *ptr_tileMap)
 {
+    UnloadTexture(&ptr_tileMap->mapTileSet);
 
+    free(ptr_tileMap->dst_rects);
+    free(ptr_tileMap->src_rects);
+    free(ptr_tileMap->isSolid);
 }
